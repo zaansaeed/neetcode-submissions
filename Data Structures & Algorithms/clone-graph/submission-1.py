c2a7d3b68e2cd3fa4d3d76500c91aa38038d9ff8
Zaan Saeed <77564853+zaansaeed@users.noreply.github.com>
2026-09-16 20:36:19 -0700

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.py
@@ -0,0 +1,40 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return
+
+        adjList = defaultdict(list)
+        q = deque([node])
+        seen = set()
+
+        while q:
+            node = q.popleft()
+            adjList[node.val] = []
+            seen.add(node)
+            for neighbor in node.neighbors:
+                    adjList[node.val].append(neighbor.val)
+                    if neighbor not in seen:
+                        q.append(neighbor)
+        
+        nodes = {k: Node(val = k) for k in adjList.keys()}
+
+        for key, neighbors in adjList.items():
+            node = nodes[key]
+            for neighbor in neighbors:
+                node.neighbors.append(nodes[neighbor])
+        
+        return nodes[1]
+
+                
+                
+
+
+        
