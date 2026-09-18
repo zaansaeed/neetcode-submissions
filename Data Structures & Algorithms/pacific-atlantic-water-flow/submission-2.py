c94f6987db189b6781175e961002c4b52d9381d6
Zaan Saeed <77564853+zaansaeed@users.noreply.github.com>
2026-09-17 17:39:58 -0700

    Add: pacific-atlantic-water-flow - submission-2

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-2.py
@@ -0,0 +1,48 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        nRows = len(heights)
+        nCols = len(heights[0])
+        matrix = [[0] * nCols for _ in range(nRows)]
+
+
+        def dfs(i, j, prev, seen):
+            if not 0<=i<nRows or not 0<=j<nCols:
+                return
+            if heights[i][j] < prev:
+                return 
+            if (i,j) in seen:
+                return
+            seen.add((i,j))
+            matrix[i][j] += 1
+            
+            dfs(i+1, j, heights[i][j], seen)
+            dfs(i-1, j, heights[i][j], seen)
+            dfs(i, j+1, heights[i][j], seen)
+            dfs(i, j-1, heights[i][j], seen)
+        visited_pac = set()
+        for idx in range(nRows):
+            dfs(idx, 0, 0, visited_pac)
+        for col in range(nCols):
+            dfs(0, col, 0, visited_pac)
+   
+        visited_atl = set()
+        for idx in range(nRows):
+            dfs(idx, nCols-1, 0, visited_atl)
+        for col in range(nCols):
+            dfs(nRows-1, col, 0, visited_atl)
+
+        return [[r,c] for r,c in visited_pac & visited_atl]
+        
+        
+        
+
+
+
+
+        
+        
+            
+
+
+
+
